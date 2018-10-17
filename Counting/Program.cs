c547306da65c87ab207bc86f1136c8ac92827297
AndrewJJT <jjtan9886@gmail.com>
2018-10-17 09:58:27 -0700

    initial commit

--- Counting/Program.cs
+++ Counting/Program.cs
@@ -10,6 +10,10 @@ namespace Counting
     {
         static void Main(string[] args)
         {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
